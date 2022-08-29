5201d66bf47983044c2bff07c0eb1926a1d5f19a
HyeonJaeGil <now9728@kaist.ac.kr>
2022-08-29 21:08:12 +0900

    sophus, ceres, and g2o installed image

---
+++ slam_base/build.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+docker build --no-cache --force-rm -f Dockerfile -t hyeonjaegil/v-slam:base .
---
+++ slam_base/run.sh
@@ -0,0 +1,10 @@
+docker run -it \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --runtime=nvidia \
+    --volume="/home/hj/Dropbox:/root/Dropbox" \
+    hyeonjaegil/v-slam:base \
+    bash
