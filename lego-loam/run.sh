11998a1a544c95d9a5f714023840e5380627a350
HyeonJaeGil <now9728@kaist.ac.kr>
2022-08-29 21:07:00 +0900

    lego loam images with gtsam 4.0.0

---
+++ lego-loam/run.sh
@@ -0,0 +1,12 @@
+docker run -it \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --env="ROS_MASTER_URI=http://172.17.0.1:11311" \
+    --env="ROS_IP=172.17.0.2" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    --runtime=nvidia \
+    --volume="/home/hj/Dropbox/dataset:/root/dataset" \
+    hyeonjaegil/lego-loam:melodic \
+    bash
