35e0b2ad83af26ac4457c13c904f118f01b3794b
HyeonJaeGil <now9728@gmail.com>
2022-12-07 00:02:05 +0900

    small fix in lego loam images

--- lego-loam/run.sh
+++ lego-loam/run.sh
@@ -1,12 +1,8 @@
-docker run -it \
-    --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --env="ROS_MASTER_URI=http://172.17.0.1:11311" \
-    --env="ROS_IP=172.17.0.2" \
+docker run --gpus all --rm -it --ipc=host --net=host --privileged \
+    --env="DISPLAY=unix$DISPLAY" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --env="XAUTHORITY=$XAUTH" \
-    --volume="$XAUTH:$XAUTH" \
     --runtime=nvidia \
-    --volume="/home/hj/Dropbox/dataset:/root/dataset" \
+    --volume="/home/hj/Dropbox/Dataset/lego-loam:/root/dataset" \
+    -v /etc/localtime:/etc/localtime:ro \
     hyeonjaegil/lego-loam:melodic \
-    bash
+    /bin/bash
